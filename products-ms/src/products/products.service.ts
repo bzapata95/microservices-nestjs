e53f890127f60a41bf6a120a4b5d77ff0debd8e7
bzapata95 <bmzc95@gmail.com>
2025-11-09 15:01:34 -0500

    config microservices

---
+++ products-ms/src/products/products.service.ts
@@ -0,0 +1,81 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsServices');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Database connected');
+  }
+
+  async create(createProductDto: CreateProductDto) {
+    const product = await this.product.create({
+      data: {
+        name: createProductDto.name,
+        price: createProductDto.price,
+      },
+    });
+
+    return product;
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalRows = await this.product.count({ where: { available: true } });
+    const lastPage = Math.ceil(totalRows / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta: {
+        page,
+        total: totalRows,
+        lastPage,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id, available: true },
+    });
+
+    if (!product) {
+      throw new NotFoundException();
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+
+    return await this.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return await this.product.update({
+      where: { id },
+      data: { available: false },
+    });
+  }
+}
